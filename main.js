5e7893c398c3df7697733d96217f2efe3060bd12
Carles Galí Bou <carles@MacBook-Pro-de-Carles.local>
2020-01-16 19:23:56 +0100

    Linked with HTML and CSS

--- main.js
+++ main.js
@@ -1,78 +1,54 @@
-/* Done with CLASS and METHOD but without linked with the HTML and CSS document */
-
-
-
 class Chrono {
     constructor() {
         this.counterSec = 0;
         this.counterMin = 0;
+        this.counterHours = 0;
         this.interID = 0;
+        
     }
 
     start() {
-       this.interID = setInterval(() => {        //we are saying that setInterval is a var with the name "chrono
+        let sec = document.getElementById("sec");   
+        let min = document.getElementById("min");
+        let hour = document.getElementById("hour");
+
+        this.interID = setInterval(() => {        
             console.log(this.counterMin, this.counterSec);
-            this.counterSec ++;     //the initialization of the chrono
-            if (this.counterSec === 60) {    //conditions to pass seconds to minutes and reset seconds if pass 60sec
+            this.counterSec ++;     
+            if (this.counterSec === 60) {    
                 this.counterMin ++;
                 this.counterSec = 0;
-            }      
-        }, 1000);   //every second this function is executed
+                if(this.counterMin === 60) {
+                    this.counterHours ++;
+                    this.counterMin = 0;
+                    if(this.counterHours === 24) {
+                        this.counterHours = 0;
+                    }
+                }
+            }
+            sec.innerHTML = this.counterSec;     
+            min.innerHTML = this.counterMin;
+            hour.innerHTML = this.counterHours;
+        }, 1000);   
     }
 
     stop() {
-        clearInterval(this.interID);  //function to stop the chrono (setInterval)
+        clearInterval(this.interID);  
     };
 
     reset() {
         clearInterval(this.interID);
         this.counterSec = 0;
         this.counterMin = 0;
-        time.start();
+        this.counterHours = 0;
+        sec.innerHTML = this.counterSec;     
+        min.innerHTML = this.counterMin;
+        hour.innerHTML = this.counterHours;
     }
 
     showTime() {
-        alert(`Min: ${this.counterMin} and sec: ${this.counterSec}`);
+        alert(`Hour: ${this.counterHours} Min: ${this.counterMin} and Sec: ${this.counterSec}`);
     }
 }
 
 let time = new Chrono();
-time.start();
-
-/* Done with FUNCTIONS and linked with the HTML and CSS document */
-
-
-
-// let chrono;
-
-// function stop() {
-//     clearInterval(chrono);  //function to stop the chrono (setInterval)
-// }
-
-// function start() {
-
-//     let counterSec = 0;     //var for counter the seconds and the minutes
-//     let counterMin = 0;
-//     let counterHour = 0;
-//     let sec = document.getElementById("sec");   //var for call the id from DOM for interact with it
-//     let min = document.getElementById("min");
-//     let hour = document.getElementById("hour")
-
-//     chrono = setInterval(() => {        //we are saying that setInterval is a var with the name "chrono
-//             if (counterSec === 60) {    //conditions to pass seconds to minutes and reset seconds if pass 60sec
-//                     counterMin ++;
-//                     counterSec = 0;
-//                 if (counterMin === 60) {    //conditions to reset minutes if pass 60min and pass minutes to hours
-//                     counterHour ++;
-//                     counterMin = 0;
-//                     if (counterHour === 24) {    //conditions to reset hours if it pass 24hours
-//                         counterHour = 0;
-//                     }
-//                 }
-//             }
-//             sec.innerHTML = counterSec;     //system to link the var counters with the counter of DOM that before we call it in a var
-//             min.innerHTML = counterMin;
-//             hour.innerHTML = counterHour;
-//             counterSec ++;     //the initialization of the chrono
-//     }, 1000);   //every second this function is executed
-// }
